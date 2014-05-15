6e084f333cf286333c3ae9b41e27f8e158442b31
Eddy Ndizera <eddy.ndizera@student.uclouvain.be>
2014-05-15 10:54:46 +0200

    writev en fonction du temps total

--- benchmark/writev/writev.c
+++ benchmark/writev/writev.c
@@ -5,16 +5,17 @@
 
 #include "benchmark.h"
 
-#define SIZE  1024 // taille du buffer
-#define FILE_SIZE1  1048576 //tailles des fichier
-#define FILE_SIZE2  4194304
+#define SIZE  512 // taille du buffer
+#define MAX   4096 //multiplicateur maximum pour la taille du buffer
+#define FILE_SIZE1  33554432 //32mb
+#define FILE_SIZE2  67108864  //64mb
 
 
 /*
  *Benchmark de writev.
  * Prends un descipteur de fichier 'fd', une taille de buffer 'buffer_size', une taille de fichier 'file_size',
  * un pointeur vers un timer 't' et un pointeur vers un recorder 'rec'.
- * Calcule le temps moyen pris pour écrire un bloc de taille 'buffer_size'.
+ * Calcule le temps moyen pris pour écrire fichier de taille 'file_size'.
  */
 void benchmark_writev(int fd, int buffer_size, int file_size, timer *t, recorder *rec){
 	int iovcnt = file_size / buffer_size;
@@ -30,7 +31,7 @@ void benchmark_writev(int fd, int buffer_size, int file_size, timer *t, recorder
 
 	start_timer(t);
 	ssize_t err = writev(fd, iov, iovcnt);
-	write_record_n(rec, buffer_size, stop_timer(t), iovcnt);
+	write_record(rec, buffer_size, stop_timer(t));
 
 	if(err == 1){
 		perror("writev");
@@ -46,7 +47,7 @@ void benchmark_writev(int fd, int buffer_size, int file_size, timer *t, recorder
  * Benchmark de lseek+write.
  * Prends un descipteur de fichier 'fd', une taille de buffer 'buffer_size', une taille de fichier 'file_size',
  * un pointeur vers un timer 't' et un pointeur vers un recorder 'rec'.
- * Calcule le temps moyen pris pour écrire un bloc de taille 'buffer_size'.
+ * Calcule le temps pris pour écrire un fichier de taille 'file_size'.
  */
 void benchmark_lseek(int fd, int buffer_size, int file_size, timer *t, recorder *rec){
 	int len = sizeof(char) * buffer_size;
@@ -59,7 +60,8 @@ void benchmark_lseek(int fd, int buffer_size, int file_size, timer *t, recorder 
 		lseek(fd, i * buffer_size, SEEK_SET); //positionne le curseur a la fin du fichier
 		write(fd, s, buffer_size);
 	}
-	write_record_n(rec, buffer_size, stop_timer(t), num);
+	write_record(rec, buffer_size, stop_timer(t));
+
 	free(s);
 }
 
@@ -74,14 +76,14 @@ int main(int argc, char *argv[]){
 	/*BENCHMARK DE WRITEV*/
 	int fd = 0; 
 	int i = 0;	
-	for(i = 1; i <= 256; i = 2*i){
+	for(i = 1; i <= MAX; i = 2*i){
 		fd = creat("tmp1", 0700);
 		benchmark_writev(fd, SIZE * i, FILE_SIZE1, t, writev_rec);
 		close(fd);
 	}
 	
 	
-	for(i = 1; i <= 256; i = 2*i){
+	for(i = 1; i <= MAX; i = 2*i){
 		fd = creat("tmp1", 0700);
 		benchmark_writev(fd, SIZE * i, FILE_SIZE2, t, writev2_rec);
 		close(fd);
@@ -90,14 +92,14 @@ int main(int argc, char *argv[]){
 
 	/*BENCHMARK DE LSEEK + WRITE*/
 
-	for(i = 1; i <= 256; i = 2*i){
+	for(i = 1; i <= MAX; i = 2*i){
 		fd = creat("tmp2", 0700);
 		benchmark_lseek(fd, SIZE * i, FILE_SIZE1, t, lseek_rec);
 		close(fd);
 	}	
 
 	
-	for(i = 1; i <= 256; i = 2*i){
+	for(i = 1; i <= MAX; i = 2*i){
 		fd = creat("tmp2", 0700);
 		benchmark_lseek(fd, SIZE * i, FILE_SIZE2, t, lseek2_rec);
 		close(fd);
