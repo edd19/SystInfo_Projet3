fd41df91ed15634c737295f234001d84994f0b2b
Eddy Ndizera <eddy.ndizera@student.uclouvain.be>
2014-05-15 13:14:10 +0200

    fix 1024 for iovec

--- benchmark/writev/writev.c
+++ benchmark/writev/writev.c
@@ -5,8 +5,8 @@
 
 #include "benchmark.h"
 
-#define SIZE  512 // taille du buffer
-#define MAX   4096 //multiplicateur maximum pour la taille du buffer
+#define SIZE  65536 // taille du buffer
+#define MAX   512 //multiplicateur maximum pour la taille du buffer
 #define FILE_SIZE1  33554432 //32mb
 #define FILE_SIZE2  67108864  //64mb
 
@@ -25,6 +25,10 @@ void benchmark_writev(int fd, int buffer_size, int file_size, timer *t, recorder
 	
 	for(i = 0; i < iovcnt; i++){
 		iov[i].iov_base = malloc(iov_len);
+		if(iov[i].iov_base == NULL){
+			fprintf(stderr, "malloc at benchmark_writev");
+			exit(0);
+		}
 		memset(iov[i].iov_base, 0, iov_len);
 		iov[i].iov_len = iov_len;
 	}// remplit les iovec par des '0'
@@ -33,9 +37,10 @@ void benchmark_writev(int fd, int buffer_size, int file_size, timer *t, recorder
 	ssize_t err = writev(fd, iov, iovcnt);
 	write_record(rec, buffer_size, stop_timer(t));
 
-	if(err == 1){
-		perror("writev");
-		exit(0);
+	if(err == -1){
+		printf("iovcnt : %d \n", iovcnt);
+		//perror("writev");		
+		//exit(0);
 	}
 
 	for(i = 0; i < iovcnt; i++){
@@ -53,12 +58,21 @@ void benchmark_lseek(int fd, int buffer_size, int file_size, timer *t, recorder 
 	int len = sizeof(char) * buffer_size;
 	int num = file_size / buffer_size;	
 	char *s = malloc(len);
+	if(s == NULL){
+		fprintf(stderr, "malloc at benchmark_lseek");
+		exit(0);
+	}
 	memset(s, 0, len); //remplit la chaîne de caractere par des '0'
 	int i = 0;
+	int err = 0;
 	start_timer(t);
 	for(i = 0; i < num; i++){
 		lseek(fd, i * buffer_size, SEEK_SET); //positionne le curseur a la fin du fichier
-		write(fd, s, buffer_size);
+		err = write(fd, s, buffer_size);
+		if(err == -1){
+			perror("write");
+			exit(0);
+		}
 	}
 	write_record(rec, buffer_size, stop_timer(t));
 
